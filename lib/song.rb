dc7f1e66d1e1512f4bb9e83de149cb637c18c393
Tyler Firth <tyfirth@gmail.com>
2020-06-07 00:31:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     @@all << self
   end
 
-  def artist_name=(name)
+  def artist_name= (name)
     self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
   end
