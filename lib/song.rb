f6057e3acc37b73b494c85c72e9d45fbfaaef62a
Tyler Firth <tyfirth@gmail.com>
2020-06-07 00:31:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,12 @@ class Song
     self.artist.add_song(self)
   end
   
- 
+   def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
   
   def self.all 
     @@all 
